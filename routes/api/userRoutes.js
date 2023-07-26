386c1ceb4dea2083c3717eb0fb66d01ee96b2379
Samimi <smokhadar@gmail.com>
2023-07-25 17:51:04 -0700

    added user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    updateUser,
+    addNewFriend,
+    deleteFriend
+} = require('../../controllers/userControls');
+
+// api/users
+router.route('/').get(getUsers).post(createUser);
+
+//api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+//api/users/:userId/friends
+router.route('/:userId/friends').post(addNewFriend);
+
+//api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
+
+module.exports = router;
